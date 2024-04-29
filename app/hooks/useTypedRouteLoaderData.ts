a556e1985cc20f5daabc19c28d51f295d6ee9e88
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-04-29 12:51:27 +0200

    Fjerne session modal debug

--- app/hooks/useTypedRouteLoaderData.ts
+++ app/hooks/useTypedRouteLoaderData.ts
@@ -1,18 +1,16 @@
 import { useRouteLoaderData } from "@remix-run/react";
-import type { loader as RootLoader } from "~/root";
+import type { loader as rootLoader } from "~/root";
 import type { loader as indexLoader } from "~/routes/_index";
 
 type Loaders = {
-  root: typeof RootLoader;
+  root: typeof rootLoader;
   "routes/_index": typeof indexLoader;
 };
 
 export function useTypedRouteLoaderData<T extends keyof Loaders>(route: T) {
   const routeData = useRouteLoaderData<Loaders[T]>(route);
 
-  console.log(`🔥 route :`, route);
   if (!routeData) {
-    console.log(`route error :`, route);
     throw new Error(
       "Route data is not loaded. You might be trying to accessing data from a sub route that has not yet loaded"
     );
