93c82afd32a134f1ec28bf7754b0925309b4bc4a
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-05-02 13:24:24 +0200

    Oppdatere webserver url for playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -47,7 +47,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run dev",
-    url: baseURL,
+    url: "http://localhost:5173",
     reuseExistingServer: !process.env.CI,
     timeout: 60000,
   },
