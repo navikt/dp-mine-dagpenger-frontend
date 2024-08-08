99f8319bbbae9f2c2b251369e550f413f9bd6e13
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-08-08 13:06:17 +0200

    Debug saf audience

--- app/utils/auth.utils.server.ts
+++ app/utils/auth.utils.server.ts
@@ -33,7 +33,7 @@ export async function getSAFToken(request: Request) {
     return getEnv("SAF_TOKEN") || "";
   }
 
-  const audience = `${getEnv("SAF_AUDIENCE")}:teamdokumenthandtering:safselvbetjening`;
+  const audience = `${getEnv("SAF_AUDIENCE")}:dev-fss:teamdokumenthandtering:safselvbetjening-q1`;
   return await getOnBehalfOfToken(request, audience);
 }
 
