418057de4c3d14280540d84f720ed0cbb4b4da90
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-09-12 16:27:21 +0200

    Oppdatere sanity query og bruker kopi-av-prod datasett

--- app/sanity/sanity.query.ts
+++ app/sanity/sanity.query.ts
@@ -16,15 +16,15 @@ const linkFields = `{
 }`;
 
 export const appTextsGroq = `* [_type=="mineDagpengerAppText" && language==$baseLang]{
-    ...coalesce(* [textId==^.textId && language==$lang][0]${appTextsFields}, ${appTextsFields})
+  ...coalesce(* [textId==^.textId && language==$lang][0]${appTextsFields}, ${appTextsFields})
 }`;
 
 const infoTextsGroq = `* [_type=="mineDagpengerRichText" && language==$baseLang]{
-  ...coalesce(* [textId==^.textId && language==$lang][0]${infoTextsFields}, ${infoTextsFields})
+  ...coalesce(* [slug==^.slug && language==$lang][0]${infoTextsFields}, ${infoTextsFields})
 }`;
 
 const linksGroq = `* [_type=="mineDagpengerLink" && language==$baseLang]{
-  ...coalesce(* [textId==^.textId && language==$lang][0]${linkFields}, ${linkFields})
+  ...coalesce(* [linkId==^.linkId && language==$lang][0]${linkFields}, ${linkFields})
 }`;
 
 export const allTextsQuery = `{
