080ebb145d247a9150cc87f29949168945538fe6
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-11-11 14:34:52 +0100

    Rename amplitude event navn

--- app/utils/amplitude.ts
+++ app/utils/amplitude.ts
@@ -3,7 +3,7 @@ import { getAmplitudeInstance } from "@navikt/nav-dekoratoren-moduler";
 const logger = getAmplitudeInstance("dekoratoren");
 
 export function loggLastned(dokumentId: string) {
-  logger("Dokument nedlastet", {
+  logger("lastet ned dokument", {
     dokumentId,
   });
 }
