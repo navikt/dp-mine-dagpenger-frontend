45b4057a09748c00cd9a4959bea58e58fd1242c4
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2025-01-28 09:54:05 +0100

    CSP: tilater uxsignals api url
    Co-authored-by: John Martin Lindseth <john.martin.lindseth@nav.no>

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -43,6 +43,7 @@ const csp = {
     "https://telemetry.ekstern.dev.nav.no/collect",
     "https://telemetry.nav.no/collect",
     "https://in2.taskanalytics.com/",
+    "https://api.uxsignals.com",
   ],
 };
 let cspString = `connect-src ${csp["connect-src"].join(" ")}; img-src ${csp["img-src"].join(" ")};`;
