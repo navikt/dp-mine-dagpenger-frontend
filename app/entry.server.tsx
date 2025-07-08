04ef5cab1671eadb45e4c480d9aada2498172520
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2025-07-08 16:14:22 +0200

    Revert "Oppdatere csp mot XSS for entry.server.ts"
    This reverts commit 93f52373a9671726461145b5164c0cd129131565.

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -51,10 +51,6 @@ export default function handleRequest(
           const stream = createReadableStreamFromReadable(body);
 
           responseHeaders.set("Content-Type", "text/html");
-          responseHeaders.set(
-            "Content-Security-Policy",
-            "default-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none';"
-          );
 
           resolve(
             new Response(stream, {
