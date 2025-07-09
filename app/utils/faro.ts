5c055762196afa037cfc6880250d7e241b184d71
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2025-07-09 09:36:28 +0200

    Legge til funksjon for å hindre Regex injection for faro cors url

--- app/utils/faro.ts
+++ app/utils/faro.ts
@@ -2,6 +2,13 @@ import { type Faro, getWebInstrumentations, initializeFaro } from "@grafana/faro
 import { TracingInstrumentation } from "@grafana/faro-web-tracing";
 import { getEnv } from "~/utils/env.utils";
 
+// EscapeRegExp sørger for at alle spesialtegn i en streng blir escaped før bruk i RegExp.
+// Dette forhindrer Regular Expression Injection, som kan føre til sikkerhetsproblemer.
+// Se: https://owasp.org/www-community/attacks/Regular_expression_Denial_of_Service_-_ReDoS
+function escapeRegExp(str: string) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
+
 let faro: Faro | null = null;
 
 export function initFaro() {
@@ -30,11 +37,11 @@ export function initFaro() {
       new TracingInstrumentation({
         instrumentationOptions: {
           propagateTraceHeaderCorsUrls: [
-            new RegExp(`${getEnv("DP_SOKNADSDIALOG_URL")}/*`),
-            new RegExp(`${getEnv("DP_INNSYN_URL")}/*`),
-            new RegExp(`${getEnv("OKONOMI_KONTOREGISTER_URL")}/*`),
-            new RegExp(`${getEnv("PAW_ARBEIDSSOEKERREGISTERET_URL")}/*`),
-            new RegExp(`${getEnv("SAF_URL")}/*`),
+            new RegExp(`${escapeRegExp(getEnv("DP_SOKNADSDIALOG_URL"))}/.*`),
+            new RegExp(`${escapeRegExp(getEnv("DP_INNSYN_URL"))}/.*`),
+            new RegExp(`${escapeRegExp(getEnv("OKONOMI_KONTOREGISTER_URL"))}/.*`),
+            new RegExp(`${escapeRegExp(getEnv("PAW_ARBEIDSSOEKERREGISTERET_URL"))}/.*`),
+            new RegExp(`${escapeRegExp(getEnv("SAF_URL"))}/.*`),
           ],
         },
       }),
