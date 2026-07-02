f62e436db5f14c9070aecd4ea7f69929443eaeb0
ArselanSultani <arselan.sultani@nav.no>
2026-07-02 09:58:39 +0200

    La på litt status på behandlingstid på søknader innsendt de siste 8 ukene (#719)

--- app/components/soknad-list/FullforteSoknadList.tsx
+++ app/components/soknad-list/FullforteSoknadList.tsx
@@ -1,10 +1,13 @@
 import { useSanity } from "~/hooks/useSanity";
 import { useRouteLoaderData } from "react-router";
-import { Alert } from "@navikt/ds-react";
+import { Alert, InfoCard } from "@navikt/ds-react";
 import styles from "~/components/soknad-list/SoknadList.module.css";
 import { getSoknadWithinLast12WeeksOrkestrator } from "~/utils/soknad.utils";
 import { ISoknad } from "~/models/getSoknader.server";
 import { FullforteSoknad } from "~/components/soknad-list/FullforteSoknad";
+import { isAfter, subWeeks } from "date-fns";
+import { NyesteInnsendtSøknadStatus } from "~/components/soknad-list/NyesteInnsendtSøknadStatus";
+import { LightBulbIcon } from "@navikt/aksel-icons";
 
 export function FullforteSoknadList() {
   const { getAppText } = useSanity();
@@ -22,12 +25,41 @@ export function FullforteSoknadList() {
     .filter((soknad: ISoknad) => soknad.søknadId)
     .filter((soknad: ISoknad) => soknad.status === "INNSENDT" || soknad.status === "JOURNALFØRT");
 
-  const fullforteSoknaderWithin12Weeks = getSoknadWithinLast12WeeksOrkestrator(alleSoknader);
+  const fullforteSoknaderWithin12Weeks = getSoknadWithinLast12WeeksOrkestrator(alleSoknader).sort((a, b) => {
+    const dateA = new Date(a.innsendtTimestamp);
+    const dateB = new Date(b.innsendtTimestamp);
+    return dateB.getTime() - dateA.getTime();
+  });
 
-  if (soknader.status === "success" && fullforteSoknaderWithin12Weeks.length > 0) {
+  if (fullforteSoknaderWithin12Weeks.length < 1) {
+    return <></>;
+  }
+
+  const nyesteSøknad = isAfter(new Date(fullforteSoknaderWithin12Weeks[0].innsendtTimestamp), subWeeks(new Date(), 8))
+    ? fullforteSoknaderWithin12Weeks[0]
+    : null;
+
+
+  if (soknader.status === "success") {
     return (
       <ul className={styles.soknadList}>
-        {fullforteSoknaderWithin12Weeks.map((soknad) => (
+        {
+          nyesteSøknad ? (
+            <>
+              <NyesteInnsendtSøknadStatus soknad={nyesteSøknad} key={nyesteSøknad.søknadId} />
+              <InfoCard data-color="info" className={styles.soknadInfoBox}>
+                <InfoCard.Header icon={<LightBulbIcon aria-hidden />}>
+                  <InfoCard.Title>Saksbehandlingstid</InfoCard.Title>
+                </InfoCard.Header>
+                <InfoCard.Content>
+                  En vanlig grunn til lang saksbehandlingstid er at vi ikke har fått all dokumentasjonen
+                  vi trenger fra deg. Du kan gjerne dobbeltsjekke at du har sendt inn alt vi trenger.
+                </InfoCard.Content>
+              </InfoCard>
+            </>
+          ) : null
+        }
+        {fullforteSoknaderWithin12Weeks.filter(soknad => soknad.søknadId != nyesteSøknad?.søknadId).map((soknad) => (
           <FullforteSoknad soknad={soknad} key={soknad.søknadId} />
         ))}
       </ul>
---
+++ app/components/soknad-list/NyesteInnsendtSøknadStatus.tsx
@@ -0,0 +1,60 @@
+import { ISoknad } from "~/models/getSoknader.server";
+import { useSanity } from "~/hooks/useSanity";
+import { getEnv } from "~/utils/env.utils";
+import styles from "~/components/soknad-list/SoknadList.module.css";
+import { BodyShort, Heading } from "@navikt/ds-react";
+import { FormattedDate } from "~/components/FormattedDate";
+import { ExternalLink } from "~/components/ExternalLink";
+import { addWeeks } from "date-fns";
+
+
+interface IProps {
+  soknad: ISoknad;
+}
+
+export function NyesteInnsendtSøknadStatus({ soknad }: IProps) {
+  const { søknadId, tittel, innsendtTimestamp } = soknad;
+  const { getAppText } = useSanity();
+
+  const ettersendingUrl = `${getEnv("DP_BRUKERDIALOG_URL")}/${søknadId}/ettersending`;
+  const kvitteringUrl = `${getEnv("DP_BRUKERDIALOG_URL")}/${søknadId}/kvittering`;
+  const innsendtDato = new Date(innsendtTimestamp);
+  const estimertSvarFraDato = addWeeks(innsendtDato, 6);
+  const estimertSvarTilDato = addWeeks(innsendtDato, 7);
+
+  return (
+    <div className={styles.soknadContainer}>
+      <article className={styles.soknadContent}>
+        <Heading level="3" size="small">
+          {tittel}
+        </Heading>
+        <div className={styles.soknadStatusText}>
+          <BodyShort className={styles.soknadDate} size="small">
+            Vi har mottatt søknaden din om dagpenger:
+          </BodyShort>
+          <BodyShort className={styles.soknadStatusDate}>
+            <FormattedDate date={innsendtTimestamp} bareDato={true} />
+          </BodyShort>
+        </div>
+        <div>
+          <BodyShort className={styles.soknadDate} size="small">
+            Saksbehandlingstiden er for tiden 6 uker. Derfor tror vi at du vil få svar fra oss en gang mellom
+          </BodyShort>
+          <BodyShort className={styles.soknadStatusDate}>
+            <FormattedDate date={estimertSvarFraDato.toString()} bareDato={true} /> {" og "}
+            <FormattedDate date={estimertSvarTilDato.toString()} bareDato={true} />
+          </BodyShort>
+        </div>
+      </article>
+      <nav className={styles.soknadLinksContainer}>
+        <ExternalLink to={ettersendingUrl} asButtonVariant="primary" size="small">
+          {getAppText("fullfort-soknad.send-dokumentasjon.knapp-tekst")}
+        </ExternalLink>
+        <ExternalLink to={kvitteringUrl} asButtonVariant="secondary" size="small">
+          {getAppText("fullfort-soknad.se-soknad.knapp-tekst")}
+        </ExternalLink>
+      </nav>
+    </div>
+  );
+
+}
