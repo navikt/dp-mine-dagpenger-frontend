6d79c5ed800f6d8988333401ce6affc591e49da0
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2026-09-08 14:59:50 +0200

    Bumpe minor og patches, oppdater vite og vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "./app"),
+      "~": path.resolve(import.meta.dirname, "./app"),
     },
   },
   test: {
