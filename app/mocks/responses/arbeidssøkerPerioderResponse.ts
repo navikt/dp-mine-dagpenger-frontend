53eef51508e3211e68c24d6e6c07e7b182d03266
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2026-09-09 13:37:39 +0200

    Rename arbeidssøker til norsk

--- app/mocks/responses/arbeidssøkerPerioderResponse.ts
+++ app/mocks/responses/arbeidssøkerPerioderResponse.ts
@@ -1,6 +1,6 @@
 import type { Arbeidssøkerperioder } from "~/models/hentArbeidssøkerStatus.server";
 
-export const arbeidssoekerPerioderResponse: Arbeidssøkerperioder[] = [
+export const arbeidssøkerPerioderResponse: Arbeidssøkerperioder[] = [
   {
     periodeId: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
     startet: {
