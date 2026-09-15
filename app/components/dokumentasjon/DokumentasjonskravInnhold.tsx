2c94d3a603500b4e5d9648a55cfad97f882f44ec
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2026-09-15 09:56:20 +0200

    Bedre fallback tekst

--- app/components/dokumentasjon/DokumentasjonskravInnhold.tsx
+++ app/components/dokumentasjon/DokumentasjonskravInnhold.tsx
@@ -11,15 +11,15 @@ export function DokumentasjonskravInnhold({ type }: IProps) {
     return (
       <VStack gap="space-8">
         <BodyLong>{beskrivelse}</BodyLong>
-          <VStack gap="space-8">
-            <BodyShort>Du må legge ved dokumentasjon som viser</BodyShort>
-            <List as="ul">
-              <List.Item>hvilken pengestøtte dette er</List.Item>
-              <List.Item>hvem som utbetaler pengestøtten</List.Item>
-              <List.Item>hvor mye du får</List.Item>
-              <List.Item>perioden den utbetales for</List.Item>
-            </List>
-          </VStack>
+        <VStack gap="space-8">
+          <BodyShort>Du må legge ved dokumentasjon som viser</BodyShort>
+          <List as="ul">
+            <List.Item>hvilken pengestøtte dette er</List.Item>
+            <List.Item>hvem som utbetaler pengestøtten</List.Item>
+            <List.Item>hvor mye du får</List.Item>
+            <List.Item>perioden den utbetales for</List.Item>
+          </List>
+        </VStack>
       </VStack>
     );
   }
@@ -41,25 +41,25 @@ export function DokumentasjonskravInnhold({ type }: IProps) {
               Du har lagt til et arbeidsforhold. Du må dokumentere arbeidsforholdet ved å sende oss
               arbeidsavtalen.
             </BodyShort>
-              <VStack gap="space-8">
-                <BodyShort>Arbeidsavtalen må inneholde</BodyShort>
-                <List as="ul">
-                  <List.Item>datoen du startet i jobben din</List.Item>
-                  <List.Item>stillingsprosent eller avtalt arbeidstid</List.Item>
-                  <List.Item>sluttdato, hvis du har en midlertidig arbeidsavtale</List.Item>
-                </List>
-                <BodyLong>
-                  Arbeidsavtalen må inneholde datoen du startet i jobben din stillingsprosent eller
-                  avtalt arbeidstid avt endt oppsigelsestid sluttdato, hvis du har en midlertidig
-                  arbeidsavtale Hvis du ikke har arbeidsavtalen din, kan arbeidsgiveren din fylle ut
-                  skjemaet{" "}
-                  <ExternalLink
-                    to="https://cdn.sanity.io/files/gx9wf39f/soknadsveiviser-p/25dc1dbc777af7749d8c6efbb439839694b04dd5.pdf"
-                    title="Bekreftelse på sluttårsak eller nedsatt arbeidstid (04-08.03)"
-                  />
-                  . Du kan også be arbeidsgiveren din bekrefte opplysningene på en annen måte.
-                </BodyLong>
-              </VStack>
+            <VStack gap="space-8">
+              <BodyShort>Arbeidsavtalen må inneholde</BodyShort>
+              <List as="ul">
+                <List.Item>datoen du startet i jobben din</List.Item>
+                <List.Item>stillingsprosent eller avtalt arbeidstid</List.Item>
+                <List.Item>sluttdato, hvis du har en midlertidig arbeidsavtale</List.Item>
+              </List>
+              <BodyLong>
+                Arbeidsavtalen må inneholde datoen du startet i jobben din stillingsprosent eller
+                avtalt arbeidstid avt endt oppsigelsestid sluttdato, hvis du har en midlertidig
+                arbeidsavtale Hvis du ikke har arbeidsavtalen din, kan arbeidsgiveren din fylle ut
+                skjemaet{" "}
+                <ExternalLink
+                  to="https://cdn.sanity.io/files/gx9wf39f/soknadsveiviser-p/25dc1dbc777af7749d8c6efbb439839694b04dd5.pdf"
+                  title="Bekreftelse på sluttårsak eller nedsatt arbeidstid (04-08.03)"
+                />
+                . Du kan også be arbeidsgiveren din bekrefte opplysningene på en annen måte.
+              </BodyLong>
+            </VStack>
           </VStack>
         );
 
@@ -67,24 +67,23 @@ export function DokumentasjonskravInnhold({ type }: IProps) {
         return (
           <VStack gap="space-8">
             <BodyShort>Du har krysset av for at du har blitt sagt opp.</BodyShort>
-              <VStack gap="space-8">
-                <BodyShort>Du legge ved dokumentasjon som viser</BodyShort>
-                <List as="ul">
-                  <List.Item>datoen du fikk oppsigelsen</List.Item>
-                  <List.Item>årsaken til at du ble sagt opp</List.Item>
-                </List>
-                <BodyLong>
-                  For å dokumentere dette, kan du legge ved oppsigelsen du fikk av arbeidsgiveren
-                  din. Hvis oppsigelsen ikke inneholder opplysningene vi trenger, kan du bruke
-                  skjemaet.{" "}
-                  <ExternalLink
-                    to="https://cdn.sanity.io/files/gx9wf39f/soknadsveiviser-p/25dc1dbc777af7749d8c6efbb439839694b04dd5.pdf"
-                    title=" Bekreftelse på sluttårsak eller nedsatt arbeidstid (04-08.03)"
-                  />
-                  . Arbeidsgiveren din må fylle ut og signere skjemaet. Du kan også be arbeidsgiveren
-                  din om å dokumentere opplysningene på en annen måte.
-                </BodyLong>
-              </VStack>
+            <VStack gap="space-8">
+              <BodyShort>Du legge ved dokumentasjon som viser</BodyShort>
+              <List as="ul">
+                <List.Item>datoen du fikk oppsigelsen</List.Item>
+                <List.Item>årsaken til at du ble sagt opp</List.Item>
+              </List>
+              <BodyLong>
+                For å dokumentere dette, kan du legge ved oppsigelsen du fikk av arbeidsgiveren din.
+                Hvis oppsigelsen ikke inneholder opplysningene vi trenger, kan du bruke skjemaet.{" "}
+                <ExternalLink
+                  to="https://cdn.sanity.io/files/gx9wf39f/soknadsveiviser-p/25dc1dbc777af7749d8c6efbb439839694b04dd5.pdf"
+                  title=" Bekreftelse på sluttårsak eller nedsatt arbeidstid (04-08.03)"
+                />
+                . Arbeidsgiveren din må fylle ut og signere skjemaet. Du kan også be arbeidsgiveren
+                din om å dokumentere opplysningene på en annen måte.
+              </BodyLong>
+            </VStack>
           </VStack>
         );
 
@@ -92,23 +91,23 @@ export function DokumentasjonskravInnhold({ type }: IProps) {
         return (
           <VStack gap="space-8">
             <BodyShort>Du har krysset av for at du har sagt opp jobben.</BodyShort>
-              <VStack gap="space-8">
-                <BodyShort>Du legge ved dokumentasjon som viser</BodyShort>
-                <List as="ul">
-                  <List.Item>datoen du ga oppsigelsen til arbeidsgiveren din</List.Item>
-                  <List.Item>årsaken til at du sa opp jobben din</List.Item>
-                </List>
-                <BodyLong>
-                  For å dokumentere dette, kan du legge ved oppsigelsen du ga arbeidsgiveren din.
-                  Hvis oppsigelsen ikke inneholder opplysningene vi trenger, kan du bruke skjemaet{" "}
-                  <ExternalLink
-                    to="https://cdn.sanity.io/files/gx9wf39f/soknadsveiviser-p/25dc1dbc777af7749d8c6efbb439839694b04dd5.pdf"
-                    title=" Bekreftelse på sluttårsak eller nedsatt arbeidstid (04-08.03)"
-                  />
-                  . Arbeidsgiveren din må fylle ut og signere skjemaet. Du kan også be arbeidsgiveren
-                  din om å dokumentere opplysningene på en annen måte.
-                </BodyLong>
-              </VStack>
+            <VStack gap="space-8">
+              <BodyShort>Du legge ved dokumentasjon som viser</BodyShort>
+              <List as="ul">
+                <List.Item>datoen du ga oppsigelsen til arbeidsgiveren din</List.Item>
+                <List.Item>årsaken til at du sa opp jobben din</List.Item>
+              </List>
+              <BodyLong>
+                For å dokumentere dette, kan du legge ved oppsigelsen du ga arbeidsgiveren din. Hvis
+                oppsigelsen ikke inneholder opplysningene vi trenger, kan du bruke skjemaet{" "}
+                <ExternalLink
+                  to="https://cdn.sanity.io/files/gx9wf39f/soknadsveiviser-p/25dc1dbc777af7749d8c6efbb439839694b04dd5.pdf"
+                  title=" Bekreftelse på sluttårsak eller nedsatt arbeidstid (04-08.03)"
+                />
+                . Arbeidsgiveren din må fylle ut og signere skjemaet. Du kan også be arbeidsgiveren
+                din om å dokumentere opplysningene på en annen måte.
+              </BodyLong>
+            </VStack>
           </VStack>
         );
 
@@ -116,24 +115,24 @@ export function DokumentasjonskravInnhold({ type }: IProps) {
         return (
           <VStack gap="space-8">
             <BodyShort>Du har krysset av for at du har blitt avskjediget.</BodyShort>
-              <VStack gap="space-8">
-                <BodyShort>Du legge ved dokumentasjon som viser</BodyShort>
-                <List as="ul">
-                  <List.Item>datoen du ble avskjediget</List.Item>
-                  <List.Item>årsaken til at du ble avskjediget</List.Item>
-                </List>
-                <BodyLong>
-                  For å dokumentere dette, kan du sende oss avskjedigelsen du har fått fra
-                  arbeidsgiver. Hvis avskjedigelsen ikke inneholder opplysningene vi trenger, kan du
-                  bruke skjemaet{" "}
-                  <ExternalLink
-                    to="https://cdn.sanity.io/files/gx9wf39f/soknadsveiviser-p/25dc1dbc777af7749d8c6efbb439839694b04dd5.pdf"
-                    title=" Bekreftelse på sluttårsak eller nedsatt arbeidstid (04-08.03)"
-                  />
-                  . Arbeidsgiveren din må fylle ut og signere skjemaet. Du kan også be arbeidsgiveren
-                  din om å dokumentere opplysningene på en annen måte.
-                </BodyLong>
-              </VStack>
+            <VStack gap="space-8">
+              <BodyShort>Du legge ved dokumentasjon som viser</BodyShort>
+              <List as="ul">
+                <List.Item>datoen du ble avskjediget</List.Item>
+                <List.Item>årsaken til at du ble avskjediget</List.Item>
+              </List>
+              <BodyLong>
+                For å dokumentere dette, kan du sende oss avskjedigelsen du har fått fra
+                arbeidsgiver. Hvis avskjedigelsen ikke inneholder opplysningene vi trenger, kan du
+                bruke skjemaet{" "}
+                <ExternalLink
+                  to="https://cdn.sanity.io/files/gx9wf39f/soknadsveiviser-p/25dc1dbc777af7749d8c6efbb439839694b04dd5.pdf"
+                  title=" Bekreftelse på sluttårsak eller nedsatt arbeidstid (04-08.03)"
+                />
+                . Arbeidsgiveren din må fylle ut og signere skjemaet. Du kan også be arbeidsgiveren
+                din om å dokumentere opplysningene på en annen måte.
+              </BodyLong>
+            </VStack>
           </VStack>
         );
 
@@ -141,23 +140,23 @@ export function DokumentasjonskravInnhold({ type }: IProps) {
         return (
           <VStack gap="space-8">
             <BodyShort>Du har krysset av for at arbeidstiden din er redusert.</BodyShort>
-              <VStack gap="space-8">
-                <BodyShort>Du legge ved dokumentasjon som viser</BodyShort>
-                <List as="ul">
-                  <List.Item>datoen arbeidstiden din ble redusert</List.Item>
-                  <List.Item>årsaken til at arbeidstiden din ble redusert</List.Item>
-                  <List.Item>hvor mye arbeidstiden din ble redusert</List.Item>
-                </List>
-                <BodyLong>
-                  Du må be arbeidsgiver om denne dokumentasjonen. Hvis dokumentene ikke inneholder
-                  opplysningene vi trenger, kan du bruke skjemaet{" "}
-                  <ExternalLink
-                    to="https://cdn.sanity.io/files/gx9wf39f/soknadsveiviser-p/25dc1dbc777af7749d8c6efbb439839694b04dd5.pdf"
-                    title=" Bekreftelse på sluttårsak eller nedsatt arbeidstid (04-08.03)"
-                  />
-                  . Arbeidsgiveren din må fylle ut og signere skjemaet.
-                </BodyLong>
-              </VStack>
+            <VStack gap="space-8">
+              <BodyShort>Du legge ved dokumentasjon som viser</BodyShort>
+              <List as="ul">
+                <List.Item>datoen arbeidstiden din ble redusert</List.Item>
+                <List.Item>årsaken til at arbeidstiden din ble redusert</List.Item>
+                <List.Item>hvor mye arbeidstiden din ble redusert</List.Item>
+              </List>
+              <BodyLong>
+                Du må be arbeidsgiver om denne dokumentasjonen. Hvis dokumentene ikke inneholder
+                opplysningene vi trenger, kan du bruke skjemaet{" "}
+                <ExternalLink
+                  to="https://cdn.sanity.io/files/gx9wf39f/soknadsveiviser-p/25dc1dbc777af7749d8c6efbb439839694b04dd5.pdf"
+                  title=" Bekreftelse på sluttårsak eller nedsatt arbeidstid (04-08.03)"
+                />
+                . Arbeidsgiveren din må fylle ut og signere skjemaet.
+              </BodyLong>
+            </VStack>
           </VStack>
         );
 
@@ -168,17 +167,15 @@ export function DokumentasjonskravInnhold({ type }: IProps) {
               Du har krysset av for at arbeidsgiveren din er konkurs. Du må legge ved brevet du har
               fått fra bostyreren eller konkursforvalteren.
             </BodyShort>
-              <VStack gap="space-8">
-                <BodyShort>Du legge ved dokumentasjon som viser</BodyShort>
-                <List as="ul">
-                  <List.Item>at arbeidsgiveren din er konkurs</List.Item>
-                  <List.Item>datoen konkursen er åpnet</List.Item>
-                  <List.Item>at boet ikke trer inn i arbeidsavtalen din</List.Item>
-                </List>
-                <BodyLong>
-                  Brevet må være signert av bostyreren eller konkursforvalteren
-                </BodyLong>
-              </VStack>
+            <VStack gap="space-8">
+              <BodyShort>Du legge ved dokumentasjon som viser</BodyShort>
+              <List as="ul">
+                <List.Item>at arbeidsgiveren din er konkurs</List.Item>
+                <List.Item>datoen konkursen er åpnet</List.Item>
+                <List.Item>at boet ikke trer inn i arbeidsavtalen din</List.Item>
+              </List>
+              <BodyLong>Brevet må være signert av bostyreren eller konkursforvalteren</BodyLong>
+            </VStack>
           </VStack>
         );
 
@@ -186,33 +183,30 @@ export function DokumentasjonskravInnhold({ type }: IProps) {
         return (
           <VStack gap="space-8">
             <BodyShort>
-              Du har krysset av for at du er permittert. Du må legge ved permitteringsvarselet som du
-              har fått fra arbeidsgiveren din.
+              Du har krysset av for at du er permittert. Du må legge ved permitteringsvarselet som
+              du har fått fra arbeidsgiveren din.
             </BodyShort>
-              <VStack gap="space-8">
-                <BodyShort>Permitteringsvarselet må inneholde informasjon om</BodyShort>
-                <List as="ul">
-                  <List.Item>årsaken til at du ble permittert (permitteringsårsak)</List.Item>
-                  <List.Item>hvor mange prosent du er permittert (permitteringsgrad)</List.Item>
-                  <List.Item>startdato og eventuell sluttdato for permitteringen</List.Item>
-                  <List.Item>datoen du fikk permitteringsvarselet</List.Item>
-                  <List.Item>
-                    om partene på arbeidsplassen din er enige om permitteringen (JA/NEI)
-                  </List.Item>
-                </List>
-                <BodyShort>
-                  Det må komme frem at permitteringen gjelder deg. skjemaet.
-                </BodyShort>
-                <BodyLong>
-                  Hvis permitteringsvarselet ikke inneholder disse opplysningene kan du bruke
-                  skjemaet{" "}
-                  <ExternalLink
-                    to="https://cdn.sanity.io/files/gx9wf39f/soknadsveiviser-p/4f473293d31eee48921daecc72b1157e2a06542f.pdf"
-                    title=" Bekreftelse på arbeidsforhold og permittering (NAV 04-08.04)"
-                  />
-                  . Arbeidsgiveren din må fylle ut og signere skjemaet.
-                </BodyLong>
-              </VStack>
+            <VStack gap="space-8">
+              <BodyShort>Permitteringsvarselet må inneholde informasjon om</BodyShort>
+              <List as="ul">
+                <List.Item>årsaken til at du ble permittert (permitteringsårsak)</List.Item>
+                <List.Item>hvor mange prosent du er permittert (permitteringsgrad)</List.Item>
+                <List.Item>startdato og eventuell sluttdato for permitteringen</List.Item>
+                <List.Item>datoen du fikk permitteringsvarselet</List.Item>
+                <List.Item>
+                  om partene på arbeidsplassen din er enige om permitteringen (JA/NEI)
+                </List.Item>
+              </List>
+              <BodyShort>Det må komme frem at permitteringen gjelder deg. skjemaet.</BodyShort>
+              <BodyLong>
+                Hvis permitteringsvarselet ikke inneholder disse opplysningene kan du bruke skjemaet{" "}
+                <ExternalLink
+                  to="https://cdn.sanity.io/files/gx9wf39f/soknadsveiviser-p/4f473293d31eee48921daecc72b1157e2a06542f.pdf"
+                  title=" Bekreftelse på arbeidsforhold og permittering (NAV 04-08.04)"
+                />
+                . Arbeidsgiveren din må fylle ut og signere skjemaet.
+              </BodyLong>
+            </VStack>
           </VStack>
         );
 
@@ -220,8 +214,8 @@ export function DokumentasjonskravInnhold({ type }: IProps) {
         return (
           <BodyLong>
             Du har krysset av for at du jobbet rotasjon. Du må dokumentere hvilken rotasjonsordning
-            du har hatt. Den siste rotasjonen må dokumenteres med timeliste eller lignende, som viser
-            når du var på jobb og når du hadde avspasering.
+            du har hatt. Den siste rotasjonen må dokumenteres med timeliste eller lignende, som
+            viser når du var på jobb og når du hadde avspasering.
           </BodyLong>
         );
 
@@ -299,7 +293,9 @@ export function DokumentasjonskravInnhold({ type }: IProps) {
 
       default:
         console.error(`Ukjent dokumentasjonskrav: ${type}`);
-        return <p>Ukjent dokumentasjonskrav.</p>;
+        return (
+          <p>Vi klarte ikke å vise hvilken dokumentasjon du må sende inn. Ta kontakt med Nav.</p>
+        );
     }
   }
 
