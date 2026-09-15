a8f2b4ba3f539ce93f1e4889b25dbfa2f45c77a6
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2026-09-15 15:17:34 +0200

    Vise saksbehandlignstid i readmore, legge til info kort om brev

--- app/components/soknad-list/FullforteSoknadList.tsx
+++ app/components/soknad-list/FullforteSoknadList.tsx
@@ -1,17 +1,13 @@
-import { useSanity } from "~/hooks/useSanity";
-import { useRouteLoaderData } from "react-router";
 import { Alert, InfoCard } from "@navikt/ds-react";
-import styles from "~/components/soknad-list/SoknadList.module.css";
-import { getSoknadWithinLast12WeeksOrkestrator } from "~/utils/soknad.utils";
-import { ISoknad } from "~/models/getSoknader.server";
-import { FullforteSoknad } from "~/components/soknad-list/FullforteSoknad";
 import { isAfter, subWeeks } from "date-fns";
+import { useRouteLoaderData } from "react-router";
+import { FullforteSoknad } from "~/components/soknad-list/FullforteSoknad";
 import { NyesteInnsendtSøknadStatus } from "~/components/soknad-list/NyesteInnsendtSøknadStatus";
-import { LightBulbIcon } from "@navikt/aksel-icons";
-import {
-  filtrerSoknaderTilVisning,
-  skalViseSaksbehandlingstid,
-} from "./saksbehandlingstid.utils";
+import styles from "~/components/soknad-list/SoknadList.module.css";
+import { useSanity } from "~/hooks/useSanity";
+import { ISoknad } from "~/models/getSoknader.server";
+import { getSoknadWithinLast12WeeksOrkestrator } from "~/utils/soknad.utils";
+import { filtrerSoknaderTilVisning, skalViseSaksbehandlingstid } from "./saksbehandlingstid.utils";
 
 export function FullforteSoknadList() {
   const { getAppText } = useSanity();
@@ -29,19 +25,24 @@ export function FullforteSoknadList() {
     .filter((soknad: ISoknad) => soknad.søknadId)
     .filter((soknad: ISoknad) => soknad.status === "INNSENDT" || soknad.status === "JOURNALFØRT");
 
-  const fullforteSoknaderWithin12Weeks = getSoknadWithinLast12WeeksOrkestrator(alleSoknader).sort((a, b) => {
-    const dateA = new Date(a.innsendtTimestamp);
-    const dateB = new Date(b.innsendtTimestamp);
-    return dateB.getTime() - dateA.getTime();
-  });
+  const fullforteSoknaderWithin12Weeks = getSoknadWithinLast12WeeksOrkestrator(alleSoknader).sort(
+    (a, b) => {
+      const dateA = new Date(a.innsendtTimestamp);
+      const dateB = new Date(b.innsendtTimestamp);
+      return dateB.getTime() - dateA.getTime();
+    }
+  );
 
   if (fullforteSoknaderWithin12Weeks.length < 1) {
     return null;
   }
 
   const nyesteInnsendtTidspunkt = new Date(fullforteSoknaderWithin12Weeks[0].innsendtTimestamp);
-  const estimertSaksbehandlingstid = 7
-  const nyesteSøknad = isAfter(nyesteInnsendtTidspunkt, subWeeks(new Date(), estimertSaksbehandlingstid + 2))
+  const estimertSaksbehandlingstid = 7;
+  const nyesteSøknad = isAfter(
+    nyesteInnsendtTidspunkt,
+    subWeeks(new Date(), estimertSaksbehandlingstid + 2)
+  )
     ? fullforteSoknaderWithin12Weeks[0]
     : null;
 
@@ -54,9 +55,9 @@ export function FullforteSoknadList() {
 
   if (soknader.status === "success") {
     return (
-      <ul className={styles.soknadList}>
-        {
-          nyesteSøknad && (
+      <>
+        <ul className={styles.soknadList}>
+          {nyesteSøknad && (
             <>
               {visSaksbehandlingstid && (
                 <NyesteInnsendtSøknadStatus
@@ -65,25 +66,24 @@ export function FullforteSoknadList() {
                   estimertSaksbehandlingstid={estimertSaksbehandlingstid}
                 />
               )}
-              {visSaksbehandlingstid && (
-                <InfoCard data-color="info" className={styles.soknadInfoBox}>
-                  <InfoCard.Header icon={<LightBulbIcon aria-hidden />}>
-                    <InfoCard.Title>Saksbehandlingstid</InfoCard.Title>
-                  </InfoCard.Header>
-                  <InfoCard.Content>
-                    Vi behandler søknaden din så snart vi kan, og når du har sendt all dokumentasjonen
-                    vi trenger. Det er mange søknader som skal behandles nå, og vi beklager ventetiden.
-                    Du får beskjed så snart søknaden din er ferdig behandlet.
-                  </InfoCard.Content>
-                </InfoCard>
-              )}
             </>
-          )
-        }
-        {soknaderTilVisning.map((soknad) => (
-          <FullforteSoknad soknad={soknad} key={soknad.søknadId} />
-        ))}
-      </ul>
+          )}
+          {soknaderTilVisning.map((soknad) => (
+            <FullforteSoknad soknad={soknad} key={soknad.søknadId} />
+          ))}
+        </ul>
+
+        <InfoCard data-color="info" className={styles.soknadInfoBox}>
+          <InfoCard.Header>
+            <InfoCard.Title>Saksbehandlingstid</InfoCard.Title>
+          </InfoCard.Header>
+          <InfoCard.Content>
+            Vi behandler søknaden din så snart vi kan, og når du har sendt all dokumentasjonen vi
+            trenger. Det er mange søknader som skal behandles nå, og vi beklager ventetiden. Du får
+            beskjed så snart søknaden din er ferdig behandlet.
+          </InfoCard.Content>
+        </InfoCard>
+      </>
     );
   }
 
--- app/components/soknad-list/NyesteInnsendtSøknadStatus.tsx
+++ app/components/soknad-list/NyesteInnsendtSøknadStatus.tsx
@@ -1,13 +1,23 @@
-import { ISoknad } from "~/models/getSoknader.server";
-import { useSanity } from "~/hooks/useSanity";
-import { getEnv } from "~/utils/env.utils";
-import styles from "~/components/soknad-list/SoknadList.module.css";
-import { BodyShort, Box, Heading, HStack, ReadMore, Tag, VStack } from "@navikt/ds-react";
-import { FormattedDate } from "~/components/FormattedDate";
-import { ExternalLink } from "~/components/ExternalLink";
+import {
+  BodyShort,
+  Box,
+  Heading,
+  HStack,
+  InfoCard,
+  Link,
+  ReadMore,
+  Tag,
+  VStack,
+} from "@navikt/ds-react";
 import { addWeeks } from "date-fns";
 import { Dokumentasjonskrav } from "~/components/dokumentasjon/dokumentasjon.types";
 import { DokumentasjonskravInnhold } from "~/components/dokumentasjon/DokumentasjonskravInnhold";
+import { ExternalLink } from "~/components/ExternalLink";
+import { FormattedDate } from "~/components/FormattedDate";
+import styles from "~/components/soknad-list/SoknadList.module.css";
+import { useSanity } from "~/hooks/useSanity";
+import { ISoknad } from "~/models/getSoknader.server";
+import { getEnv } from "~/utils/env.utils";
 
 interface IProps {
   soknad: ISoknad;
@@ -37,69 +47,84 @@ export function NyesteInnsendtSøknadStatus({ soknad, estimertSaksbehandlingstid
         <Heading level="3" size="medium" spacing>
           {tittel}
         </Heading>
-        <div className={styles.soknadStatusText}>
-          <BodyShort className={styles.soknadDate} size="small">
-            Vi har mottatt søknaden din om dagpenger:
-          </BodyShort>
-          <BodyShort className={styles.soknadStatusDate}>
-            <FormattedDate date={innsendtTimestamp} bareDato={true} />
-          </BodyShort>
-        </div>
-        <div>
-          <BodyShort className={styles.soknadDate} size="small">
-            Saksbehandlingstiden er for tiden {estimertSaksbehandlingstid} uker. Derfor tror vi at
-            du vil få svar fra oss en gang mellom
-          </BodyShort>
-          <BodyShort className={styles.soknadStatusDate}>
-            <FormattedDate date={estimertSvarFraDato.toString()} bareDato={true} /> {" og "}
-            <FormattedDate date={estimertSvarTilDato.toString()} bareDato={true} />
-          </BodyShort>
-        </div>
-      </article>
-      <nav className={styles.soknadLinksContainer}>
-        <ExternalLink to={ettersendingUrl} asButtonVariant="primary" size="small">
-          {getAppText("fullfort-soknad.send-dokumentasjon.knapp-tekst")}
-        </ExternalLink>
-        <ExternalLink to={kvitteringUrl} asButtonVariant="secondary" size="small">
-          {getAppText("fullfort-soknad.se-soknad.knapp-tekst")}
-        </ExternalLink>
-        <ExternalLink to={nySøknadUrl} asButtonVariant="secondary" size="small">
-          Send ny søknad
-        </ExternalLink>
-      </nav>
-      {manglendeDokumentasjonskrav.length > 0 && (
-        <VStack padding="space-16" gap="space-16">
-          {manglendeDokumentasjonskrav.map((krav) => (
-            <Box padding="space-16" key={krav.id} background="sunken" borderRadius="8">
-              <VStack gap="space-12">
-                <HStack justify="space-between" wrap={false} align="start">
-                  <Heading size="xsmall" level="4">
-                    {krav.tittel}
-                  </Heading>
-                  <Tag variant="warning" size="xsmall">
-                    Mangler
-                  </Tag>
-                </HStack>
-                <BodyShort color="subtle" size="small">
-                  Frist{" "}
-                  <FormattedDate
-                    date={ettersendingFrist.toString()}
-                    bareDato={true}
-                    utenÅrstall={true}
-                  />
-                </BodyShort>
-                <ReadMore header={"Dette må dokumentasjonen inneholde"}>
-                  <DokumentasjonskravInnhold type={krav.type} />
-                </ReadMore>
-              </VStack>
-            </Box>
-          ))}
-        </VStack>
-      )}
+        <BodyShort className={styles.soknadDate} size="small">
+          Vi har mottatt søknaden din om dagpenger:
+        </BodyShort>
+        <BodyShort className={styles.soknadStatusDate}>
+          <FormattedDate date={innsendtTimestamp} bareDato={true} />
+        </BodyShort>
+        <BodyShort className={styles.soknadDate} size="small">
+          Saksbehandlingstiden er for tiden {estimertSaksbehandlingstid} uker. Derfor tror vi at du
+          vil få svar fra oss en gang mellom
+        </BodyShort>
+        <BodyShort className={styles.soknadStatusDate} spacing>
+          <FormattedDate date={estimertSvarFraDato.toString()} bareDato={true} /> {" og "}
+          <FormattedDate date={estimertSvarTilDato.toString()} bareDato={true} />
+        </BodyShort>
 
-      <nav className={styles.soknadLinksContainerForSkyra}>
-        <skyra-survey slug="arbeids-og-velferdsetaten-nav/mine-dagpenger-status-i-sak"></skyra-survey>
-      </nav>
+        <ReadMore header="Saksbehandlingstid">
+          Vi behandler søknaden din så snart vi kan, og når du har sendt all dokumentasjonen vi
+          trenger. Det er mange søknader som skal behandles nå, og vi beklager ventetiden. Du får
+          beskjed så snart søknaden din er ferdig behandlet.
+        </ReadMore>
+
+        <HStack gap="space-8" className="mt-4">
+          <ExternalLink to={ettersendingUrl} asButtonVariant="primary" size="small">
+            {getAppText("fullfort-soknad.send-dokumentasjon.knapp-tekst")}
+          </ExternalLink>
+          <ExternalLink to={kvitteringUrl} asButtonVariant="secondary" size="small">
+            {getAppText("fullfort-soknad.se-soknad.knapp-tekst")}
+          </ExternalLink>
+          <ExternalLink to={nySøknadUrl} asButtonVariant="secondary" size="small">
+            Send ny søknad
+          </ExternalLink>
+        </HStack>
+
+        {manglendeDokumentasjonskrav.length > 0 && (
+          <VStack gap="space-16" className="mt-4">
+            {manglendeDokumentasjonskrav.map((krav) => (
+              <Box key={krav.id} className="mt-4">
+                <VStack gap="space-8">
+                  <HStack justify="space-between" wrap={false} align="start" gap="space-12">
+                    <Heading size="xsmall" level="4">
+                      {krav.tittel}
+                    </Heading>
+                    <Tag variant="warning" size="xsmall">
+                      Mangler
+                    </Tag>
+                  </HStack>
+                  <BodyShort color="subtle" size="small">
+                    Frist{" "}
+                    <FormattedDate
+                      date={ettersendingFrist.toString()}
+                      bareDato={true}
+                      utenÅrstall={true}
+                    />
+                  </BodyShort>
+                  <ReadMore header={"Dette må dokumentasjonen inneholde"}>
+                    <DokumentasjonskravInnhold type={krav.type} />
+                  </ReadMore>
+                </VStack>
+              </Box>
+            ))}
+          </VStack>
+        )}
+
+        <InfoCard data-color="info" className={styles.soknadInfoBox}>
+          <InfoCard.Header>
+            <InfoCard.Title>Har du fått brev om manglende opplysninger?</InfoCard.Title>
+          </InfoCard.Header>
+          <InfoCard.Content>
+            Hvis du har fått brev om manglende opplysninger vil det stå i brevet hva som skal sendes
+            inn og frist for å sende inn. Brev du har fått ligger i{" "}
+            <Link href="#dokumentliste">dokumentlisten på Mine dagpenger.</Link>
+          </InfoCard.Content>
+        </InfoCard>
+
+        <nav className={styles.soknadLinksContainerForSkyra}>
+          <skyra-survey slug="arbeids-og-velferdsetaten-nav/mine-dagpenger-status-i-sak"></skyra-survey>
+        </nav>
+      </article>
     </div>
   );
 }
